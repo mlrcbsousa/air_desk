e6fdf7028f9470eb797a2e247f62690f7ef8a399
chris sisserian <c.sisserian@gmail.com>
2018-12-03 16:11:22 +0000

    created office seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_150450) do
+ActiveRecord::Schema.define(version: 2018_12_03_152011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offices", force: :cascade do |t|
+    t.string "location"
+    t.string "name"
+    t.text "description"
+    t.integer "capacity"
+    t.float "dayrate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
