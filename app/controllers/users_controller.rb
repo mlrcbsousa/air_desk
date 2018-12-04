4b6c60ca81534e31949821038e08b5e0e93218c2
Thibaut de Briey <tdebriey@gmail.com>
2018-12-04 15:22:38 +0000

    changed navbar, added a search bar and changed some stuff in our controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def dashboard
     @user = current_user
     authorize @user
+    @offices = @user.offices
+    @bookings = @user.bookings
     # TODO
     # display information inside variable `current_user`
     # eg: user.offices, user.bookings
