5d1fa0d32946a4a0b97b35c6a3ea1c082011f8c9
Thibaut de Briey <tdebriey@gmail.com>
2018-12-06 18:35:36 +0000

    upda

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -36,7 +36,6 @@ class OfficesController < ApplicationController
     @office = Office.new(params_office)
     @office.user = current_user
     authorize @office
-
     if @office.save
       params[:office_attachments]['attachment'].each do |a|
         @office_attachment = @office.office_attachments.create!(attachment: a)
@@ -59,6 +58,7 @@ class OfficesController < ApplicationController
   end
 
   def update
+    authorize @office
     @office.update(params_office)
     if @office.save
       redirect_to @office, notice: 'Office was successfully updated.'
