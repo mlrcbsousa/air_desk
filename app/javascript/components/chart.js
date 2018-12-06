5ea1494885171e10de3c1a6a1dd0daa08fbf9650
chris sisserian <c.sisserian@gmail.com>
2018-12-06 13:51:36 +0000

    chart working with hard coded data

---
+++ app/javascript/components/chart.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ["Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "MTD"],
+        datasets: [{
+            label: "Bookings by month",
+            backgroundColor: '#46CEE0',
+            borderColor: 'rgb(255, 255, 255)',
+            data: [10, 5, 2, 20, 30, 25, 54, 5, 34, 49, 20],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
+export { chart };
